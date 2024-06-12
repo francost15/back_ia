eb94f4305b7784351e1386d5553ad8c6c5337ca4
Franco <ti43243@uvp.edu.mx>
2024-06-11 22:40:33 -0600

    arreglando todo

--- src/Routes/Products.routes.js
+++ src/Routes/Products.routes.js
@@ -5,7 +5,7 @@ import {verifyToken} from "../Middleware/Auth.js";
 
 const rutas = Router()
 
-rutas.get('/api/products', verifyToken, getProducts)
+rutas.get('/api/products', getProducts)
 rutas.get('/api/products/:id', verifyToken, getProducts)
 rutas.post('/api/products', verifyToken, uploadImagen.single('imagen'), saveProducts)
 rutas.put('/api/products/:id', verifyToken, uploadImagen.single('imagen'), updateProducts)
